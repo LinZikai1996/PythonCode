faed427ef03743e5f7a6937302b1efa61b38009c
Lin, Zikai <elegantrabbit@163.com>
2020-07-27 06:05:44 +0800

    pandas

--- ML_HeiMa/use_numpy/np_calculation.py
+++ ML_HeiMa/use_numpy/np_calculation.py
@@ -72,3 +72,30 @@ print(np.min(stock_c))
 
 # 最小值下标
 print(np.argmin(stock_c))
+
+# 数组运算
+a1 = np.array([1, 2, 3, 4, 5])
+
+print(a1 + 1)
+
+print(a1 / 2)
+
+print(a1 * 10)
+
+# 矩阵乘法
+a2 = np.array([[80, 86],
+               [82, 80],
+               [85, 78],
+               [90, 90],
+               [86, 82],
+               [78, 80],
+               [92, 94]])
+
+a3 = np.array([[0.7], [0.3]])
+
+print(np.matmul(a2, a3))
+
+print(np.dot(a2, a3))
+
+# np.matmul(a2, a3) 不支持矩阵和数字相乘
+print(np.dot(a2, 10))
