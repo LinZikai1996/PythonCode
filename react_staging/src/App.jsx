cd6753020b82290594631baeaf6f42115aa32629
LinZikai1996 <elegantrabbit@163.com>
2022-07-15 22:12:08 +0800

    update code

--- react_staging/src/App.jsx
+++ react_staging/src/App.jsx
@@ -1,15 +1,9 @@
 import React, { Component } from 'react'
-import Search from './component/Search'
-import List from './component/List'
 
 export default class App extends Component {
-
   render() {
     return (
-      <div className="container">
-        <Search />
-        <List />
-      </div>
+      <div>App</div>
     )
   }
 }
