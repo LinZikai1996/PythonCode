1c7f5ea0e43e4a65599e69209f4024bf8af14d83
LinZikai1996 <elegantrabbit@163.com>
2022-07-16 07:26:48 +0800

    重命名

--- react_staging/09_NavLink的使用及封装/src/App.jsx
+++ react_staging/09_NavLink的使用及封装/src/App.jsx
@@ -27,6 +27,10 @@ export default class App extends Component {
               <a className="list-group-item active" href="./home.html">Home</a> */}
 
               {/* 在React中靠路由链接实现切换组件 -- 编写路由连接 */}
+              {/* 
+              <NavLink activeClassName="atguigu" className="list-group-item" to="/about">About</NavLink>
+							<NavLink activeClassName="atguigu" className="list-group-item" to="/home">Home</NavLink> 
+              */}
               <MyNavLink to='/about'>About</MyNavLink>
               <MyNavLink to='/home'>Home</MyNavLink>
             </div>
