55436ff228b34a3ddca39b6c57a63f6bdc90ca0d
LinZikai1996 <elegantrabbit@163.com>
2023-05-01 22:44:47 +0800

    03完成,04一半

--- TypeScriptCodeLearn/02_basis_and_types.ts
+++ TypeScriptCodeLearn/02_basis_and_types.ts
@@ -56,3 +56,56 @@ g = "hello"
 // 类型断言，可以用来告诉解析器变成实际类型, 两种用法
 b = g as string;
 b = <string> g;
+
+
+// function test_function(num: any): true | 123 返回值类型自动改了
+function test_function(num: number){
+    if (num > 100){
+        return true
+    } else {
+        return 123
+    }
+}
+
+
+// void 表示没有返回值，never 表示永远不会有返回结果
+function test_void(): void{
+
+}
+
+function test_never(): never{
+    throw new Error("error");
+    
+}
+
+
+// object 表示一个js 对象
+let h: object;
+h = {};
+h = function (){}
+
+// {}用来指定对象中可以包含哪些属性，语法：{属性名： 属性值, 属性名?： 属性值}，其中问号，表示属性可选
+let i: {name: string, age?: number};
+i = {name: "zikai"}
+
+// [propName: string]: any 表示任意类型的属性
+let j: {name: string, [propName: string]: any};
+j = {
+    name: "zikai",
+    age: 18,
+    sex: "man"
+}
+
+// 设置函数结构的类型声明
+// 语法： (形参： 类型，形参： 类型 ...) => 返回值
+let k: (k1: number, k2: number) => number;
+
+k = function(n1, n2): number{
+    return n1 + n2
+}
+
+
+// l, m字符串数组, 两种方式
+let l: string[];
+l = ['a', 'b'];
+let m: Array<string>
