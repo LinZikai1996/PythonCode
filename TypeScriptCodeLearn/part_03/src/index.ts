c39ffe19049da1abe7569a33c5bb1f7710dbc275
LinZikai1996 <elegantrabbit@163.com>
2023-05-10 07:36:26 +0800

    09完成

---
+++ TypeScriptCodeLearn/part_03/src/index.ts
@@ -0,0 +1,5 @@
+function test_1(a:number, b:number):number{
+    return a + b
+}
+
+console.log(test_1(12, 34))
---
+++ TypeScriptCodeLearn/part_03/webpack.config.js
@@ -0,0 +1,32 @@
+// 引入一个包
+
+const path = require('path')
+
+// webpack 中所有配置信息都应该写在 module.exports 中
+module.exports = {
+    // 指定入口文件
+    entry: './src/index.ts',
+
+    // 指定打包输出目录
+    output: {
+        // 指定目录
+        path: path.resolve(__dirname, 'dist'),
+        // 打包后文件名字
+        filename: 'boudle.js'
+    },
+
+    // 指定webpack要打包时使用模块
+    module:{
+        // 指定加载规则
+        rules:[
+            {
+                // test 指定规则生效的文件
+                test: /\.ts$/,
+                // 指定使用的loader
+                use: 'ts-loader',
+                // 指定要排除的文件
+                exclude: /node-modules/
+            }
+        ]
+    }
+};
