9d72fe8e6e3731d2fcaef7f8414707a59fd99cfd
LinZikai1996 <elegantrabbit@163.com>
2023-05-22 21:32:50 +0800

    update code

--- React+ML/client/src/App.tsx
+++ React+ML/client/src/App.tsx
@@ -1,8 +1,27 @@
+import { createTheme } from "@mui/material/styles"
+import { useMemo } from "react"
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import { themeSettings } from "@/theme"
+import { Box, CssBaseline, ThemeProvider } from "@mui/material"
 
 function App() {
 
+  const theme = useMemo(() => createTheme(themeSettings), [])
+
   return (
-    <div className='app'></div>
+    <div className='app'>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline/>
+            <Box width="100%" height="100%" padding="1rem 2rem 4rem">
+              <Routes>
+                <Route path='/' element={<div>dashboard page</div>}></Route>
+                <Route path='/predicitons' element={<div>predictions page</div>}></Route>
+              </Routes>
+            </Box>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
   )
 }
 
